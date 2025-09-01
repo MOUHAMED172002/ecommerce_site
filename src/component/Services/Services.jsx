20562536bf8bc9772f174e79774c3c85b20c8cd9
MOUHAMED172002 <abdoul172002@gmail.com>
2025-09-01 21:00:38 +0100

    finalisation du composant services et innitialisation du composant Banners

--- src/component/Services/Services.jsx
+++ src/component/Services/Services.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
-import { FaCarSide, FaHeadphonesAll,FaWallet, FaCheckCircle } from 'react-icons/fa6'
+import { FaHeadphonesAlt } from 'react-icons/fa'
+import { FaCarSide ,FaWallet, FaCircleCheck } from 'react-icons/fa6'
+
+
+const ServiceData =[
+    {
+        id:1,
+        icon:<FaCarSide className="text-4xl md:text-5xl text-primary"/>,
+        title:'Free Shipping',
+        description: "Free Shipping On All Order"
+
+    },
+    {
+        id:2,
+        icon:<FaCircleCheck className='text-4xl md:text-5xl text-primary'/>,
+        title:'Safe Money',
+        description:'30 Days Money Back'
+    },
+    {
+        id:3,
+        icon:<FaHeadphonesAlt className='text-4xl md:text-5xl text-primary' />,
+        title:"Online Support 24/7",
+        description:"Technical Support 24/7"
+    },
+    {
+        id:4,
+        icon:<FaWallet className='text-4xl md:text-5xl text-primary'/>,
+        title:"Secure Payment",
+        description:"All payment Secure"
+    },
+    
+]
 
 const Services = () => {
   return (
     <div>
         <div className='container'>
-            <div className='grid'>
-                <div></div>
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 gap-y-8'>
+                {
+                    ServiceData.map((data)=>(
+                        <div >
+                            {data.icon}
+                            <div className='flex flex-col items-start truncate sm:flex-row gap-4 '>
+                                <h1 className='lg:text-xl font-bold'>{data.title}</h1>
+                                <h1 className='text-gray-400 text-sm'>{data.description}</h1>
+                            </div>
+                        </div>
+                    ))
+                }
+                <div>
+
+                </div>
             </div>
         </div>
     </div>
